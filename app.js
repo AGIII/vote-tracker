ee9525843212614d1c3619f1e779d80d3a315b5d
George Ascherl <georgea@cox.net>
2015-11-10 13:12:14 -0800

    new branch push

--- app.js
+++ app.js
@@ -45,3 +45,24 @@ imageRight.src = cars[imgRight].photo;
 };
 compareImages();
 
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+]
+
