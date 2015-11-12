edcb53c9d23396814722760ccdcadaf12c6148f9
George Ascherl <georgea@cox.net>
2015-11-11 18:45:34 -0800

    got chart and car names fully working

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ var CarVoteTracker =function(name, photo, color) {
   cars.push(this);
 }
 
-var img1 = new CarVoteTracker('img1', 'images/1.jpg', '#123456');
-var img2 = new CarVoteTracker('img2', 'images/2.jpg', '#234567');
-var img3 = new CarVoteTracker('img3', 'images/3.jpg');
-var img4 = new CarVoteTracker('img4', 'images/4.jpg');
-var img5 = new CarVoteTracker('img5', 'images/5.jpg');
-var img6 = new CarVoteTracker('img6', 'images/6.jpg');
-var img7 = new CarVoteTracker('img7', 'images/7.jpg');
-var img8 = new CarVoteTracker('img8', 'images/8.jpg');
-var img9 = new CarVoteTracker('img9', 'images/9.jpg');
-var img10 = new CarVoteTracker('img10', 'images/10.jpg');
-var img11 = new CarVoteTracker('img11', 'images/11.jpg');
-var img12 = new CarVoteTracker('img12', 'images/12.jpg');
+var img1 = new CarVoteTracker('img1', 'images/1.jpg', 'red');
+var img2 = new CarVoteTracker('img2', 'images/2.jpg', 'green');
+var img3 = new CarVoteTracker('img3', 'images/3.jpg', 'blue');
+var img4 = new CarVoteTracker('img4', 'images/4.jpg', 'orange');
+var img5 = new CarVoteTracker('img5', 'images/5.jpg', 'yellow');
+var img6 = new CarVoteTracker('img6', 'images/6.jpg', 'pink');
+var img7 = new CarVoteTracker('img7', 'images/7.jpg', 'purple');
+var img8 = new CarVoteTracker('img8', 'images/8.jpg', 'gray');
+var img9 = new CarVoteTracker('img9', 'images/9.jpg', 'black');
+var img10 = new CarVoteTracker('img10', 'images/10.jpg', 'brown' );
+var img11 = new CarVoteTracker('img11', 'images/11.jpg', 'white');
+var img12 = new CarVoteTracker('img12', 'images/12.jpg', 'turquoise');
 
 //Random Image Selector Function
 randomImage = function() {
@@ -31,7 +31,9 @@ var imgLeft,imgRight;
 compareImages = function() {
   //Link to HTML/DOM Setup
 var imageLeft = document.getElementById('imageLeft');
+var carOne = document.getElementById('carOne');
 var imageRight = document.getElementById('imageRight');
+var carTwo = document.getElementById('carTwo');
 
   imgLeft = randomImage();
   console.log(imgLeft);
@@ -41,7 +43,10 @@ var imageRight = document.getElementById('imageRight');
   } while (imgLeft === imgRight);
 
 imageLeft.src = cars[imgLeft].photo;
+carOne.innerHTML = cars[imgLeft].name;
 imageRight.src = cars[imgRight].photo;
+carTwo.innerHTML = cars[imgRight].name;
+
 
 };
 compareImages();
@@ -90,7 +95,7 @@ function makeChart() {
     //String - Animation easing effect
     animationEasing : "easeOutBounce",
     //Boolean - Whether we animate the rotation of the Doughnut
-    animateRotate : false,
+    animateRotate : true,
     //Boolean - Whether we animate scaling the Doughnut from the centre
     animateScale : true,
     scaleShowLabelBackdrop : true
@@ -99,6 +104,16 @@ function makeChart() {
 
 makeChart();
 
+// var jsonVote1 = function() {
+//   localStorage.setItem('jsonVote1', JSON.stringify('cars'));
+// };
+
+// var getStg = function() {
+//   var storedVote1 = localStorage.getItem('jsonVote1');
+//   console.log(storedVote1);
+//   cars = JSON.parse(storedVote1)
+// };
+
 imageLeft.addEventListener('click', function() {
   console.log(cars[imgLeft].photo);
   cars[imgLeft].votes += 1;
@@ -106,13 +121,14 @@ imageLeft.addEventListener('click', function() {
   voteFor(imgLeft.src);
   compareImages();
   makeChart();
-
-
+  // jsonVote1():
+  // getStg();
 });
 
 
 
 imageRight.addEventListener('click', function() {
+  imageRight.style.border = '1px solid red';
  console.log(cars[imgRight].photo);
  cars[imgRight].votes += 1;
  console.log(cars[imgRight].name + ' has ' + cars[imgRight].votes + ' votes.');
@@ -120,7 +136,19 @@ imageRight.addEventListener('click', function() {
  compareImages();
  makeChart();
 
-
+ // jsonVote1();
+ // getStg();
 });
+
+// if (localStorage.getItem('cars') {
+//   compareImages();
+//   retrieveData();
+//   makechart();
+// } else {
+//   starTrekTotalVotes = 0;
+//   starWarsTotalVotes = 0;
+//   storeData();
+//   makechart();
+// };
 
 
